8a05bb3014da89f70cecb92c94f64f71b44cdad3
Ricardo <rp@kitchenful.com>
2024-08-26 16:45:28 -0400

    improved examples support webdriverio as default appium webdriver
    Signed-off-by: Ricardo <rp@kitchenful.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import GptDriver from "gpt-driver-node";
 
 import {Builder} from 'selenium-webdriver';
-
+import {remote} from "webdriverio";
 
 const localLambdaTestAndroidCapabilities = {
     "lt:options": {
@@ -24,46 +24,64 @@ const localIosCapabilities = {
     'appium:automationName': 'XCUITest',
     'appium:deviceName': 'iPhone 15 Pro',
     "appium:platformVersion": "17.5",
-    "browser": ""
 };
 
-const browserstackWebDriverUrl = 'https://hub-use.browserstack.com/wd/hub'
+const wdOpts = {
+    hostname: process.env.APPIUM_HOST || 'localhost',
+    port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+    logLevel: 'info',
+    capabilities: localAndroidCapabilities,
+};
+
+
+const browserstackAppiumServerUrl = 'https://hub-use.browserstack.com/wd/hub'
 
 const lambdaTestUsername = "<lambdaTestUsername>";
 const lambdaTestAccessKey = "<lambdaTestAccessKey>";
-const lambdaTestWebDriverUrl = `http://${lambdaTestUsername}:${lambdaTestAccessKey}@mobile-hub.lambdatest.com/wd/hub`
-const localWebDriverUrl = 'http://127.0.0.1:4723'
+const lambdaTestAppiumServerUrl = `https://${lambdaTestUsername}:${lambdaTestAccessKey}@mobile-hub.lambdatest.com/wd/hub`
+const localAppiumServerUrl = 'http://127.0.0.1:4723'
+
 const gptDriverApiKey = "<api-key>"
 
-const main = async () => {
-    // local selenium webdriver init.  Run 'start' script defined in package.json
-    const driver = await new Builder()
-        .usingServer(localWebDriverUrl)
-        .withCapabilities(localAndroidCapabilities)
-        .forBrowser('')
-        .build()
+const wdLambdaTestOpts = {
+    hostname: 'mobile-hub.lambdatest.com',
+    port: 443,  // Use port 443 for HTTPS
+    logLevel: 'info',
+    protocol: 'https',  // Use HTTPS
+    path: '/wd/hub',
+    capabilities: localLambdaTestAndroidCapabilities,
+    user: lambdaTestUsername,
+    key: lambdaTestAccessKey,
+};
 
-    // gptDriver instance pointing to local webdriver
-    const gptDriver = new GptDriver({
-        apiKey: gptDriverApiKey,
-        driver,
-        severConfig: {url: localWebDriverUrl}
-    })
+const main = async () => {
+    // webdriverio pointing to local appium server. Run 'start' script defined in package.json
+    const driver = await remote(wdOpts);
+
+    // webdriverio pointing to lambdaTest appium server. Run 'start' script defined in package.json
+    // const driver = await remote(wdLambdaTestOpts);
 
     // browserstack selenium webdriver init. Run 'start-browserstack' script defined in package.json
     // const driver =  await new Builder()
-    //     .usingServer(browserstackWebDriverUrl)
+    //     .usingServer(browserstackAppiumServerUrl)
     //     .build()
 
-    // gptDriver instance pointing to BrowserStack webdriver
-    // const gptDriver = new GptDriver({
-    //     apiKey: gptDriverApiKey,
-    //     driver,
-    //     severConfig: {url: browserstackWebDriverUrl}
-    // })
+    // gptDriver instance pointing to webdriver server
+    const gptDriver = new GptDriver({
+        apiKey: gptDriverApiKey,
+        driver,
+        // Option 1) point to lambdaTest appium server
+        // severConfig: {url: lambdaTestAppiumServerUrl}
+        // Option 2) point to browserStack appium server
+        // severConfig: {url: browserstackAppiumServerUrl}
+        // Option 3) point to local appiumServer
+        severConfig: {
+            url: localAppiumServerUrl,
+        }
+    });
 
     await gptDriver.startSession();
-    await gptDriver.execute("tap on Photos and succeed if 'Keep your memories safe' is displayed");
+    await gptDriver.execute("tap on youtube");
     await gptDriver.stopSession("success")
 }
 
